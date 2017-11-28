4c224a74e9b92caa3e68a54b2bd0a911816355a0
eharriett <eharriet@villanova.edu>
2017-11-28 21:08:42 +0100

    ok this is bettar

--- main_big.cpp
+++ main_big.cpp
@@ -163,7 +163,7 @@ int main(int argc, char* argv[])
                     }
                 }
 
-                for (int h = 0; g < num_planets; h++){
+                for (int h = 0; h < num_planets; h++){
                     c = &Planets[h];
                     updatePlanet(a, c);
                 }
