c466361bd8005f26d2e96048831c379c70396b94
jackbl1 <jackbloomfeld1@gmail.com>
2017-11-29 18:35:05 +0100

    remove print

--- parallel_4.cpp
+++ parallel_4.cpp
@@ -285,7 +285,6 @@ int main(int argc, char* argv[])
         {
             if (Asteroids[g].destroyed == false)
             {
-                cout << Asteroids[g].y << endl;
                 inFile << fixed << setprecision(3) << Asteroids[g].x << " " << Asteroids[g].y << " " << Asteroids[g].xvel << " " << Asteroids[g].yvel << " " << Asteroids[g].massAst << "\n";
             }
         }
