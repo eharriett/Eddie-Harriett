a27b75cb336693154bcad7ac11bde0775de7611a
Maddee <m1martin@scu.edu>
2017-11-29 14:29:09 +0100

    finished comments
    also fixed the edge calculation

--- main_big.cpp
+++ main_big.cpp
@@ -21,9 +21,7 @@ double pos_ray;
 //class for asteroids and planets
 class Asteroid{
     public: 
-      //  Asteroid();
         Asteroid(double massin, double xin, double yin);
-      //  ~Asteroid();
         void updatePosition();
         double massAst;
         double fx, fy;
@@ -31,12 +29,6 @@ class Asteroid{
         double xvel, yvel, xaccel, yaccel;
         bool destroyed;
 };
-
-//default constructor
-//Asteroid::Asteroid(void){}
-
-//default destructor
-//Asteroid::~Asteroid(void){}
 
 //constructor
 Asteroid::Asteroid(double massin, double xin, double yin)
@@ -96,28 +88,34 @@ void updateForce(Asteroid* a, Asteroid* b)
     b->fy = b->fy - forcey;
 }
 
+//updates asteroid values based on the planets
 void updatePlanet(Asteroid* a, Asteroid* b)
 {
+    //finds slope
     double slope = (a->y - b->y)/(a->x - b->x);
     if(slope > 1 || slope < -1)
         slope = slope - trunc(slope);
 
+    //finds angle
     double angle = atan(slope); 
     double forcex = (G * a->massAst * b->massAst)/(pow(dist(a,b),2)) * cos(angle);
     double forcey = (G * a->massAst * b->massAst)/(pow(dist(a,b),2)) * sin(angle);
 
+    //updates forces for the asteroid
     a->fx = a->fx + forcex;
     a->fy = a->fy + forcey;
 }
 
 int main(int argc, char* argv[]) 
 {
+    //program isn't executed with all parameters
     if (argc < 6)
     {
         cout << "Not enough parameters" << endl;
         return 1;
     }
 
+    //checks if any inputs are less than 0
     for (int i = 0; i < argc; i++)
     {
         if (atoi(argv[i]) < 0)
@@ -127,29 +125,31 @@ int main(int argc, char* argv[])
         }
     }
 
+    //sets inputted values to corresponding variables
     num_asteroids = atoi(argv[1]);
     num_iterations = atoi(argv[2]);
     num_planets = atoi(argv[3]);
     pos_ray = atoi(argv[4]);
     seed = atoi(argv[5]);
     
+    //creates vectors for asteroids and planets
     vector<Asteroid>Asteroids;
     vector<Asteroid>Planets;
     
- //   Asteroid Asteroids[num_asteroids];
- //   Asteroid Planets[num_planets];
-
+    //creates random numbers
     default_random_engine re{seed}; 
     uniform_real_distribution<double> xdist{0.0, std::nextafter(spacewidth, std :: numeric_limits<double>::max())}; 
     uniform_real_distribution<double> ydist{0.0, std::nextafter( spaceheight, std :: numeric_limits<double>::max())}; 
     normal_distribution<double> mdist{mass, sdm};
     
+    //assigns random values for the asteroids
     for (int i = 0; i < num_asteroids; i++)
     {
         Asteroid a(mdist(re), xdist(re), ydist(re));
         Asteroids.push_back(a);
     }
 
+    //assigns random values for the planets
     for (int i = 0; i < num_planets; i++)
     {
         if(i%4 == 0)
@@ -174,18 +174,26 @@ int main(int argc, char* argv[])
         }
     }
 
+    //loop through iterations
     for(int g = 0; g < num_iterations; g++)
     {
+        //loop through all asteroids in order to update the force
         for (int i = 0; i < num_asteroids-1; i++)
         {
+            //creates pointers to an asteroid so we can pass them to other functions
             Asteroid* a;
             Asteroid* b;
             Asteroid* c;
-            if(Asteroids[i].destroyed == false)
+            
+            //checks if asteroid a is still valid
+            if (Asteroids[i].destroyed == false)
             {
                 a = &Asteroids[i];
+                
+                //checks asteroid a against all other asteroids
                 for (int j = i+1; j < num_asteroids; j++)
                 {
+                    //checks is asteroid b is still valid
                     if(Asteroids[j].destroyed == false)
                     {
                         b = &Asteroids[j];
@@ -193,6 +201,7 @@ int main(int argc, char* argv[])
                     }
                 }
 
+                //checks asteroid a against all planets
                 for (int h = 0; h < num_planets; h++)
                 {
                     c = &Planets[h];
@@ -201,46 +210,49 @@ int main(int argc, char* argv[])
             }
         }
         
+        //loop through all asteroids in order to update the positions
         for(int i = 0; i < num_asteroids; i++)
         {
             if(Asteroids[i].destroyed == false)
             {
                 updatePosition(&Asteroids[i]);
 
-                if (Asteroids[i].x <= 0)
+                //if the asteroid's x or y position is within 2 from the edge, set it to 2
+                if (Asteroids[i].x < 2)
                     Asteroids[i].x = 2;
 
-                if (Asteroids[i].y <= 0)
+                if (Asteroids[i].y < 2)
                     Asteroids[i].y = 2;
 
-                if (Asteroids[i].x >= spacewidth)
+                if (Asteroids[i].x > spacewidth - 2)
                     Asteroids[i].x = spacewidth - 2;
 
-                if (Asteroids[i].y >= spaceheight)
+                if (Asteroids[i].y > spaceheight - 2)
                     Asteroids[i].y = spaceheight - 2;
 
-                //laser
+                //checks if asteroid hit the laser
                 if (Asteroids[i].y >= pos_ray-2 && Asteroids[i].y <= pos_ray+2)
                         Asteroids[i].destroyed = true; 
             }
         }
     }
     
+    //writes to file called out.txt
     ofstream inFile;
     inFile.open("out.txt");
     if (inFile.is_open())
     {
-            for (int g = 0; g < num_asteroids; g++)
+        for (int g = 0; g < num_asteroids; g++)
+        {
+            if (Asteroids[g].destroyed == false)
             {
-                if (Asteroids[g].destroyed == false)
-                {
-                    cout << Asteroids[g].y << endl;
-                    inFile << fixed << setprecision(3) << Asteroids[g].y << " " << Asteroids[g].x << " " << Asteroids[g].xvel << " " << Asteroids[g].yvel << " " << Asteroids[g].massAst << "\n";
-                }
+                cout << Asteroids[g].y << endl;
+                inFile << fixed << setprecision(3) << Asteroids[g].y << " " << Asteroids[g].x << " " << Asteroids[g].xvel << " " << Asteroids[g].yvel << " " << Asteroids[g].massAst << "\n";
             }
-            inFile.close();
         }
-        else cout << "Unable to open file";
+        inFile.close();
+    }
+    else cout << "Unable to open file";
     
     return 0;
 }
