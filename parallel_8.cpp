3ebcb9a0a7b3e95c73ce4878b66222c9c7b8976d
eharriett <eharriet@villanova.edu>
2017-11-29 18:18:42 +0100

    pragma in that place is wrong

--- parallel_8.cpp
+++ parallel_8.cpp
@@ -161,8 +161,6 @@ int main(int argc, char* argv[])
     normal_distribution<double> mdist{mass, sdm};
     
     //assigns random values for the asteroids
-    #pragma omp parallel num_threads(8)
-    #pragma omp for
     for (int i = 0; i < num_asteroids; i++)
     {
         Asteroid a(mdist(re), xdist(re), ydist(re));
@@ -170,8 +168,6 @@ int main(int argc, char* argv[])
     }
 
     //assigns random values for the planets
-    #pragma omp parallel num_threads(8)
-    #pragma omp for
     for (int i = 0; i < num_planets; i++)
     {
         if(i%4 == 0)
